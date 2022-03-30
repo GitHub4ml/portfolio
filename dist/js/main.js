294eee0dece55d71aad4bc5dc3722c53f92f289f
GitHub4ml <m.lau@hotmail.co.uk>
2022-03-30 23:38:34 +0100

    chore(profile): new profile website

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,27 +1,81 @@
-const menuBtn = document.querySelector(".menu-btn");
-const hamburger = document.querySelector(".menu-btn__burger");
-const nav = document.querySelector(".nav");
-const menuNav = document.querySelector(".menu-nav");
-const navItems = document.querySelectorAll(".menu-nav__item");
+ jQuery(document).ready(function(){
+     
+   "use strict"
+    $('.slider').ripples({
+      dropRadius: 11,
+      perturbance: 0.01,
+       
+    });
+  
+     $(".text").typed({
+         strings:["<strong>Good </strong><strong class='primary'> things</strong>","<strong>take</strong><strong class='primary'> time!</strong>"],
+         typespeed:0,
+         loop:true
+         
+     });
+     
+    $(window).scroll(function(){
+        
+       var top = $(window).scrollTop();
+        if(top>=60){
+          $("nav").addClass('secondary'); 
+        }
+        
+        else 
+            if($("nav").hasClass('secondary')){
+                $("nav").removeClass('secondary'); 
+            }
+    });
 
-let showMenu = false;
+      $('.stacks').magnificPopup({
+      delegate: 'a', // child items selector, by clicking on it popup will open
+      type: 'image',
+      gallery: {
+      enabled: true
+       } 
+                               
+    });
+     
+      $("#education-members").owlCarousel({
+          items:3,
+          autoplay:true,
+          smartSpeed:700,
+          loop:true,
+          autoplayHoverPause:true,
+            responsive:{
+              0:{
+          
+                 items:1
+              }, 
+             480:{
+          
+                 items:2
+              }, 
+                                     
+             768:{
+          
+                 items:3
+              } 
+            }
+          
+          
+          
+      });
+     $('.counter').counterUp({
+                delay: 10,
+                time: 4000
+            });
+     
+      $("a.smooth-scroll").click(function (event) {
 
-menuBtn.addEventListener("click", toggleMenu);
+        event.preventDefault();
+        
+        var section = $(this).attr("href");
 
-function toggleMenu() {
-  if (!showMenu) {
-    hamburger.classList.add("open");
-    nav.classList.add("open");
-    menuNav.classList.add("open");
-    navItems.forEach(item => item.classList.add("open"));
-
-    showMenu = true;
-  } else {
-    hamburger.classList.remove("open");
-    nav.classList.remove("open");
-    menuNav.classList.remove("open");
-    navItems.forEach(item => item.classList.remove("open"));
-
-    showMenu = false;
-  }
-}
+        $('html, body').animate({
+            scrollTop: $(section).offset().top - -2
+        }, 1250, "easeInOutExpo");
+    });
+     new WOW().init();
+     
+ });
